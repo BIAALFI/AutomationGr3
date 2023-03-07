37b217ec31797e5ab0a21a3579576de974a54bab
Bibi <biancamircea.a92gmail.com>
2023-03-08 00:18:22 +0200

    Added last things

--- src/test/java/tests/SignInTest/SignUpTest.java
+++ src/test/java/tests/SignInTest/SignUpTest.java
@@ -1,13 +1,25 @@
 package tests.SignInTest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import tests.BaseTest;
 
+import static pages.BasePage.driver;
+import static pages.BasePage.getBaseUrl;
+
+
 public class SignUpTest extends BaseTest {
+    private static final Logger LOG = LoggerFactory.getLogger(SignUpTest.class);
+    private String newUrl = getBaseUrl()+ "Index.html";
+
 
     @Test
     public void signUp() {
+
+        LOG.info("Navigate to Sign up page");
+        driver.get(newUrl);
         Assert.assertTrue(signInPage.isLogoDisplayed(),"Logo is not displayed");
         Assert.assertTrue(signInPage.isEmailFieldDisplayed(),"Email is not displayed");
         Assert.assertTrue(signInPage.isSignInButtonDisplayed(),"Sign in is not displayed");
